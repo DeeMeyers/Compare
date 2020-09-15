0326a784ad1adea8824fd64c952dfa79f4f4c572
Dee <justdmeyers@gmail.com>
2020-09-15 12:09:54 -0400

    found better option for same thing

--- corona/settings.py
+++ corona/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stormy-lake-03816.herokuapp.com', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['stormy-lake-03816.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
