36a7bdafac1a06c3c316b21a2bf6d2e218ba34b0
Dee <justdmeyers@gmail.com>
2020-09-15 15:12:24 -0400

    pulling in national covid stats added total to homepage context

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.template.loader import get_template
-
+import requests
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+    response = requests.get('https://api.covidtracking.com/v1/us/current.json')
+    info = response.json()
+    datadict = info[0]
+    
+    return render(request, 'core/home.html', {
+        'deaths': datadict['death']
+    })
